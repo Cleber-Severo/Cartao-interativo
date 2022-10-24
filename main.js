72953332edc1175acf77bc157fd94a6659aa731e
Cléber Severo <clebersevero1998@gmail.com>
2022-10-23 22:35:51 -0300

    Adding the cards first mobile versions

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 //This is the JavaScript 
+
+const botaoConfirma = document.getElementById('confirma_formulario')
+const completo = document.querySelector('.confirma')
+
+botaoConfirma.addEventListener('click', ()=> {
+    completo.classList.toggle('.confirma-show')
+})
+
