b635e85d29a25a85660fb5bbc0417dc05e6a87e2
Cléber Severo <clebersevero1998@gmail.com>
2022-10-24 22:37:26 -0300

    finishing cards mobile design/ setting up the form styles

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
 //This is the JavaScript 
 
-const botaoConfirma = document.getElementById('confirma_formulario')
-const completo = document.querySelector('.confirma')
-
-botaoConfirma.addEventListener('click', ()=> {
-    completo.classList.toggle('.confirma-show')
-})
-
