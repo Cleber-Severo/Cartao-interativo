36a20781e98c5390d7a22ccadecf2bac8d95952b
Cléber Severo <clebersevero1998@gmail.com>
2022-10-25 23:05:53 -0300

    adding the first responsive layouts

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 //This is the JavaScript 
 
+var nome =  document.querySelector(".cartao__info")
+var entrada =  document.getElementById("cardName").value
+
+
+nome.innerHTML = "entrada de usuario" 
+
+nome.textContent = entrada
+
+function F() {
+    console.log(entrada)
+}
