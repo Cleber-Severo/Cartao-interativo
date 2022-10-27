9583ac56ecdd2e449bc20dd205556622848bfd3f
Cléber Severo <clebersevero1998@gmail.com>
2022-10-26 22:08:12 -0300

    Now all inputs are updating de card info and are required

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 //This is the JavaScript 
-function muda () {
-    var entrada =  document.getElementById("cardName").value
-    document.getElementById("cartao__inf").innerHTML = entrada;
+
+function mudaNome() {
+    document.querySelector(".cartao__container--nome").innerHTML =  document.getElementById("cardName").value;
+}
+
+function mudaNumero () {
+        document.getElementById("cartao__inf").innerHTML =  document.getElementById("cardNumber").value;
+}
+
+
+function mudaMes () {
+    document.querySelector(".cartao__container--data").innerHTML = `${document.getElementById("dateMonth").value}/${document.getElementById("dateYear").value}`;
+}
+
+
+function mudaAno () {
+    document.querySelector(".cartao__container--data").innerHTML = `${document.getElementById("dateMonth").value}/${document.getElementById("dateYear").value}`;
+}
+
+function mudaCvc () {
+    document.querySelector(".cartao-tras__cvc").innerHTML = document.getElementById("securityCode").value;
 }
